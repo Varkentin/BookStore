f743f036fcb2ee685f5fd77c5193b022176a9390
Vark <wardi@mail.ru>
2020-12-01 22:02:58 +0700

    Начало разработки способа выбора доставки

---
+++ BookStore/Store/Contractors/Field.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store.Contractors
+{
+    public abstract class Field
+    {
+        public string Lable { get; }
+
+        public string Name { get; }
+
+        public string Value { get; }
+        
+        protected Field (string lable, string name, string value)
+        {
+            Lable = lable;
+            Name = name;
+            Value = value;
+        }
+    }
+
+    public class HiddenField : Field
+    {
+        public HiddenField(string lable, string name, string value) : base(lable,name,value)
+        {
+
+        }
+    }
+
+    public class SelectionField : Field
+    {
+        public IReadOnlyDictionary<string, string> Items { get; } 
+
+        public SelectionField(string lable, string name, string value, IReadOnlyDictionary<string,string> items) 
+            : base(lable, name, value)
+        {
+            Items = items;
+        }
+    }
+}
---
+++ BookStore/Store/Contractors/IDeliveryService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store.Contractors
+{
+    public interface IDeliveryService
+    {
+        string UniqueCode { get; }
+
+        string Title { get; }
+
+        Form CreateForm(Order order);
+
+        Form MoveNext(int orderId, int step, IReadOnlyDictionary<string, string> value);
+
+    }
+}
